d548189f3284d7bf260b9147208eb3530c6fbe1d
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-11 17:22:09 -0400

    hono hello world

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,7 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Hono } from '@hono/hono';
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello, Deno!'));
+
+Deno.serve(app.fetch);
