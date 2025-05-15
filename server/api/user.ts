187ff4c3300e17f6ffe39871c2085fd9cbb86c1e
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-15 01:29:45 -0400

    user endpoints

--- server/api/user.ts
+++ server/api/user.ts
@@ -1,8 +1,41 @@
-import { Hono } from '@hono/hono';
+import { Hono } from 'hono';
+
+import { DB } from 'sqlite';
+
+import { User } from '../db/classes/user.ts';
+
+import { insertUserQuery } from '../db/queries/user.ts';
+
+import type { Email } from '../../types/types.ts';
 
 export const user = new Hono()
-  .get('/', (c) => c.json('list users'))
+  .get('/', (c) => {
+    const db = new DB(Deno.env.get('DB_PATH'), { mode: 'read' });
+
+    const users = db.query<[User]>('SELECT * FROM user');
+
+    db.close();
+
+    return c.json(users);
+  })
   .get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
-  .post('/', (c) => c.json('create a user', 201))
+  .post('/', async (c) => {
+    const body: { name: string; email: Email; password: string } = await c.req.json();
+
+    const db = new DB(Deno.env.get('DB_PATH'), { mode: 'write' });
+
+    const query = insertUserQuery(db);
+    query.execute({
+      name: body.name,
+      email: body.email,
+      password: body.password,
+      created_at: Temporal.Now.instant().toString(),
+    });
+    query.finalize();
+
+    db.close();
+
+    return c.json(`created user`, 201);
+  })
   .put('/:id', (c) => c.json(`update ${c.req.param('id')}`))
   .delete('/:id', (c) => c.json(`delete ${c.req.param('id')}`));
