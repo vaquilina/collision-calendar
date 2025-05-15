52d0ae64c72f86ddcd02814393b8f5d6ccfb9d1b
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-15 01:27:29 -0400

    set DB_PATH

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,11 @@
-import { Hono } from '@hono/hono';
+import { Hono } from 'hono';
 
 import { faker } from '@faker-js/faker';
 
-// Endpoints
 import { user } from './api/user.ts';
+
+// Set environment variables
+Deno.env.set('DB_PATH', 'db/collision-calendar.db');
 
 const app = new Hono()
   /* 404 handler */
