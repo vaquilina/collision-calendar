dd95205335bda9ae8ba36728cacb760d5ccfed26
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-16 01:16:39 -0400

    fix entry types

--- server/db/queries/user.ts
+++ server/db/queries/user.ts
@@ -1,25 +1,45 @@
-import { DB, type RowObject } from 'sqlite';
+import { DB } from 'sqlite';
 
 import type { User } from '../classes/user.ts';
 import type { Email } from '../../../types/types.ts';
 
 export const insertUserQuery = (db: DB) =>
-  db.prepareQuery<[User], RowObject, { name: string; email: Email; password: string; created_at: string }>(
+  db.prepareQuery<
+    [User],
+    { id: number; name: string; email: Email; password: string; created_at: string },
+    { name: string; email: Email; password: string; created_at: string }
+  >(
     'INSERT INTO user (name, email, password, created_at) VALUES(:name, :email, :password, :created_at)',
   );
 
 export const updateUserQuery = (db: DB) =>
-  db.prepareQuery<[User], RowObject, { name: string; email: Email; password: string }>(`
+  db.prepareQuery<
+    [User],
+    { id: number; name: string; email: Email; password: string; created_at: string },
+    { name: string; email: Email; password: string }
+  >(`
   UPDATE user
      SET name = :name, email = :email, password = :password
      WHERE id = :id
   `);
 
 export const selectUserQuery = (db: DB) =>
-  db.prepareQuery<[User], RowObject, { id: number }>('SELECT * FROM user WHERE id = :id');
+  db.prepareQuery<
+    [User],
+    { id: number; name: string; email: Email; password: string; created_at: string },
+    { id: number }
+  >('SELECT * FROM user WHERE id = :id');
 
 export const selectUsersQuery = (db: DB) =>
-  db.prepareQuery<[User], RowObject, { ids: string }>('SELECT * FROM user WHERE id in (:ids)');
+  db.prepareQuery<
+    [User],
+    { id: number; name: string; email: Email; password: string; created_at: string },
+    { ids: string }
+  >('SELECT * FROM user WHERE id in (:ids)');
 
 export const deleteUserQuery = (db: DB) =>
-  db.prepareQuery<[User], RowObject, { id: number }>('DELETE FROM user WHERE id = :id');
+  db.prepareQuery<
+    [User],
+    { id: number; name: string; email: Email; password: string; created_at: string },
+    { id: number }
+  >('DELETE FROM user WHERE id = :id');
