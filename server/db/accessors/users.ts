ba573cf24f9d670742b7f22725d13c3aacab640f
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-18 16:03:04 -0400

    add accessor to retrive all users (for auth)

---
+++ server/db/accessors/users.ts
@@ -0,0 +1,20 @@
+import { DB } from 'sqlite';
+import type { User } from '../classes/user.ts';
+import type { Email } from '../../../types/types.ts';
+
+type UserEntry = {
+  id: number;
+  name: string;
+  email: Email;
+  password: string;
+  created_at: string;
+};
+
+/** Get a list of all {@link User Users}. */
+export const getAllUsers = (): User[] => {
+  const db = new DB(Deno.env.get('DB_PATH'), { mode: 'read' });
+  const records = db.queryEntries<UserEntry>('SELECT * FROM user');
+  db.close();
+
+  return records?.map<User>((u) => ({ ...u, created_at: Temporal.Instant.from(u.created_at) })) ?? [];
+};
