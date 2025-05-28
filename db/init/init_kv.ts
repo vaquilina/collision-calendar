f8e5f8d6b1cdca6cd4ad7d0b4a5f42a6c84a37bf
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-28 19:49:22 -0400

    workaround that..works

--- db/init/init_kv.ts
+++ db/init/init_kv.ts
@@ -1,6 +1,10 @@
 import { createStorage } from 'unstorage';
 import denoKVdriver from 'unstorage/drivers/deno-kv';
+import type denoKVtypes from 'unstorage/drivers/deno-kv';
 
-//export const storage = createStorage({
-//  driver: denoKVdriver({}),
-//});
+/** workaround until the library exposes the types properly */
+const driver = denoKVdriver as unknown as typeof denoKVtypes.default;
+
+export const storage = createStorage({
+  driver: driver({}),
+});
