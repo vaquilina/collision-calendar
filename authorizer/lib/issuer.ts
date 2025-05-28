55a15b4115176cdf8a6a0949bcf2fa57329d94ef
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-28 01:20:28 -0400

    invoke sendMail in auth issuer

--- authorizer/lib/issuer.ts
+++ authorizer/lib/issuer.ts
@@ -4,24 +4,23 @@ import { PasswordUI } from '@openauthjs/openauth/ui/password';
 
 import { DB } from 'sqlite';
 
-import { processEntry } from '@collision-calendar/db/util';
 import { selectUserByEmailQuery } from '@collision-calendar/db/queries';
-import { User } from '@collision-calendar/db/classes';
+
+import { sendMail } from '@collision-calendar/api/util';
 
 import { subjects } from './subjects.ts';
 
-async function getUser(email: string) {
+function getUser(email: string) {
   const db = new DB(Deno.env.get('DB_PATH'), { mode: 'read' });
   const query = selectUserByEmailQuery(db);
 
-  const entry = query.firstEntry({ email });
+  const userEntry = query.firstEntry({ email });
   query.finalize();
   db.close();
 
-  if (!entry) return;
+  if (!userEntry) return;
 
-  const user = new User(processEntry(entry));
-  return user.id;
+  return userEntry.id;
 }
 
 export default issuer({
@@ -32,16 +31,21 @@ export default issuer({
           error_email_taken: 'This email is already taken.',
         },
         sendCode: async (email, code) => {
-          console.log(email, code);
+          await sendMail({
+            from: 'Collision Calendar',
+            to: email,
+            subject: 'password reset code',
+            content: `Your password reset confirmation code is ${code}`,
+          });
         },
       }),
     ),
   },
   subjects,
-  async success(ctx, value) {
+  success(ctx, value) {
     if (value.provider === 'password') {
       return ctx.subject('user', {
-        userID: await getUser(value.email),
+        userID: getUser(value.email),
       });
     }
     throw new Error('invalid provider');
