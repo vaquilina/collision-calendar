79244853b6f32e76b4fa7df3c95520c1d1b9c246
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-30 00:47:08 -0400

    db init tests

--- tests/init_test.ts
+++ tests/init_test.ts
@@ -1,8 +1,13 @@
-import { assertEquals } from '@std/assert';
+import { assertArrayIncludes, assertEquals, assertExists } from '@std/assert';
 
-import { ENV_VAR, initEnv } from '@collision-calendar/db/init';
+import { exists } from '@std/fs';
 
-Deno.test('environment variables', () => {
+import { DB } from 'sqlite';
+
+import { Tables } from '@collision-calendar/db/classes';
+import { ENV_VAR, initDB, initEnv, initKV } from '@collision-calendar/db/init';
+
+Deno.test('environment variables are set', () => {
   initEnv();
 
   for (const key of Object.values(ENV_VAR)) {
@@ -10,5 +15,29 @@ Deno.test('environment variables', () => {
   }
 });
 
-//Deno.test('database exists', () => { });
-//Deno.test('KV exists', () => { });
+Deno.test('sqlite db exists at expected path', async () => {
+  const db_path = Deno.env.get(ENV_VAR.DB_PATH);
+  assertExists(db_path, `${ENV_VAR.DB_PATH} not set`);
+
+  initDB();
+
+  assertEquals(await exists(db_path), true, `no db found at path: ${db_path}`);
+});
+
+Deno.test('db contains expected tables', () => {
+  initDB();
+
+  const db = new DB(Deno.env.get(ENV_VAR.DB_PATH), { mode: 'read' });
+
+  const entries = db.queryEntries<{ name: string }>(`
+    SELECT name
+    FROM sqlite_master
+    WHERE type = 'table'
+  `);
+
+  db.close();
+
+  assertArrayIncludes<string>(entries.map((e) => e.name), Tables);
+});
+
+//Deno.test('kv exists at expected path', async () => {});
