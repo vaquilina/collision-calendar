b5f655bd8e93240e4b0d0cf67e44b11b562674e9
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-30 00:12:31 -0400

    export dynamically generated array of table names

--- db/classes/mod.ts
+++ db/classes/mod.ts
@@ -10,4 +10,37 @@ import { SpaceAccess } from './space_access.ts';
 import { User } from './user.ts';
 import { Vote } from './vote.ts';
 
-export { Block, Calendar, CalendarAccess, Collision, Occupant, Proposal, Repeat, Space, SpaceAccess, User, Vote };
+const getTables = async (): Promise<string[]> => {
+  const excludeEntries = ['mod', 'base_record'];
+
+  const classesPath = import.meta.dirname;
+  if (!classesPath) throw new Error('could not resolve classes path');
+
+  const tables: string[] = [];
+  for await (const dirEntry of Deno.readDir(classesPath)) {
+    const tableName = dirEntry.name.slice(0, -3);
+    if (excludeEntries.includes(tableName)) continue;
+
+    tables.push(tableName);
+  }
+
+  return tables;
+};
+
+/** Array of table names that should exist in the database. */
+const Tables = await getTables();
+
+export {
+  Block,
+  Calendar,
+  CalendarAccess,
+  Collision,
+  Occupant,
+  Proposal,
+  Repeat,
+  Space,
+  SpaceAccess,
+  Tables,
+  User,
+  Vote,
+};
