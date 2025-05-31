d14557cc35753396c1286aaafdb7d8f39cddbf91
Vince Aquilina <vince.aquilina@protonmail.com>
2025-05-30 22:43:11 -0400

    db util test

---
+++ tests/db_util_test.ts
@@ -0,0 +1,13 @@
+import { assertInstanceOf, assertObjectMatch } from '@std/assert';
+
+import { processEntry } from '@collision-calendar/db/util';
+
+Deno.test('processEntry returns { created_at: Temporal.Instant }', () => {
+  const unprocessed_entry = { created_at: Temporal.Now.instant().toString() };
+  const expected_entry = { created_at: Temporal.Instant.from(unprocessed_entry.created_at) };
+
+  const processed_entry = processEntry(unprocessed_entry);
+
+  assertObjectMatch(processed_entry, expected_entry);
+  assertInstanceOf(processed_entry.created_at, Temporal.Instant);
+});
