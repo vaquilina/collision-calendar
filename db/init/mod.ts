6d65c1e7c83a06df0790911be506b8f2d8a3a2d2
Vince Aquilina <vince.aquilina@protonmail.com>
2025-06-01 22:31:15 -0400

    export sql for db init

--- db/init/mod.ts
+++ db/init/mod.ts
@@ -1,4 +1,4 @@
-import { initDB } from './init_db.ts';
+import { create_tables_sql, initDB } from './init_db.ts';
 import { ENV_VAR, initEnv } from './init_env.ts';
 
-export { ENV_VAR, initDB, initEnv };
+export { create_tables_sql, ENV_VAR, initDB, initEnv };
