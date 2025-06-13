12de8fee7ca704d6e37ae9926b7e21a89ae2bace
Vince Aquilina <vince.aquilina@protonmail.com>
2025-06-13 01:11:08 -0400

    break down db utils test

--- tests/db_util_test.ts
+++ tests/db_util_test.ts
@@ -2,12 +2,14 @@ import { assertInstanceOf, assertObjectMatch } from '@std/assert';
 
 import { processEntry } from '@collision-calendar/db/util';
 
-Deno.test('processEntry converts created_at field to Instant', () => {
-  const unprocessed_entry = { created_at: Temporal.Now.instant().toString() };
-  const expected_entry = { created_at: Temporal.Instant.from(unprocessed_entry.created_at) };
+Deno.test('DB: util', async (t) => {
+  await t.step('util: process entry converts created_at field to Instant', (t) => {
+    const unprocessed_entry = { created_at: Temporal.Now.instant().toString() };
+    const expected_entry = { created_at: Temporal.Instant.from(unprocessed_entry.created_at) };
 
-  const processed_entry = processEntry(unprocessed_entry);
+    const processed_entry = processEntry(unprocessed_entry);
 
-  assertObjectMatch(processed_entry, expected_entry);
-  assertInstanceOf(processed_entry.created_at, Temporal.Instant);
+    assertObjectMatch(processed_entry, expected_entry);
+    assertInstanceOf(processed_entry.created_at, Temporal.Instant);
+  });
 });
