abf664f03316f76cb7c334554285cbe00a90801b
Vince Aquilina <vince.aquilina@protonmail.com>
2025-07-10 23:40:43 -0400

    migrate zod imports

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 
 import { zValidator } from '@hono/zod-validator';
 
-import { z } from 'zod/v4';
+import * as z from 'zod';
 
 import { DB } from 'sqlite';
 
--- auth/lib/subjects.ts
+++ auth/lib/subjects.ts
@@ -1,6 +1,6 @@
 import { createSubjects } from '@openauthjs/openauth/subject';
 
-import { z } from 'zod/v4';
+import * as z from 'zod';
 
 /**
  * Defines what the access token generated at the end of the auth flow will map to.
