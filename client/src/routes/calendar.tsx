db1f8d05726316d9a15da58b30c20c6357b80cb2
Vince Aquilina <vince.aquilina@protonmail.com>
2025-07-20 00:41:23 -0400

    layout/calendar improvements

--- client/src/routes/calendar.tsx
+++ client/src/routes/calendar.tsx
@@ -1,7 +1,9 @@
+import { createEffect, createSignal } from 'solid-js';
 import { createFileRoute } from '@tanstack/solid-router';
+import { Temporal } from '@js-temporal/polyfill';
+import { CaretLeft, CaretRight, Globe, MagnifyingGlass, UserRectangle } from 'phosphor-solid-js';
 import { MiniCalendar } from '../components/calendar/MiniCalendar.tsx';
 import { MonthCalendar } from '../components/calendar/MonthCalendar.tsx';
-import { ThemeSwitcher } from '../components/ThemeSwitcher.tsx';
 import { ViewSwitcher } from '../components/calendar/ViewSwitcher.tsx';
 
 export const Route = createFileRoute('/calendar')({
@@ -15,6 +17,8 @@ function CalendarComponent() {
         <CalendarToolbar />
       </header>
       <aside>
+        <button type='button' class='new-block-button'>new block</button>
+        <TimezoneDisplay />
         <MiniCalendar />
         <div>
           <fieldset>
@@ -44,9 +48,56 @@ function CalendarComponent() {
 function CalendarToolbar() {
   return (
     <>
-      <button type='button' title='Go to today'>Today</button>
+      <button type='button' title='Search'>
+        <MagnifyingGlass />
+      </button>
+      <CalendarNavigator />
       <ViewSwitcher />
-      <ThemeSwitcher />
+      <UserDisplay />
     </>
+  );
+}
+
+function CalendarNavigator() {
+  return (
+    <div class='cal-navigator'>
+      <div>
+        <button type='button' title='Go to today'>Today</button>
+        <button id='cal-backward' type='button'>
+          <CaretLeft size={12} />
+        </button>
+        <button id='cal-forward' type='button'>
+          <CaretRight size={12} />
+        </button>
+      </div>
+      <h5>September 2025</h5>
+    </div>
+  );
+}
+
+function TimezoneDisplay() {
+  const [timezone, setTimezone] = createSignal('');
+
+  createEffect(() => {
+    setTimezone(Temporal.Now.timeZoneId);
+  });
+
+  return (
+    <h6 class='timezone-display' title='Timezone'>
+      <Globe />
+      {timezone()}
+    </h6>
+  );
+}
+
+function UserDisplay() {
+  return (
+    <div class='user-display'>
+      <div class='user-display-text'>
+        <h6>Vince Aquilina</h6>
+        <address>vince.aquilina@protonmail.com</address>
+      </div>
+      <UserRectangle size={38} weight='thin' />
+    </div>
   );
 }
