69cf5214bc5495502ef386741c0b672a3300c1f7
Vince Aquilina <vince.aquilina@protonmail.com>
2025-07-28 23:11:22 -0400

    week calendar

--- client/src/const/calendar.tsx
+++ client/src/const/calendar.tsx
@@ -1,6 +1,9 @@
 /** Labels for the days of the week. */
 export const DAYS_OF_WEEK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
+/** Hour markers for a day. */
+export const HOURS_OF_DAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
 /** Number of weeks to display in the month view. */
 export const WEEKS_IN_MONTH_VIEW = 6;
 
