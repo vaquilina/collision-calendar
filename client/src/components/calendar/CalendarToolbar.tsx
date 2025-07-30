90fcb123b6425ef7b0a3a687f5d6c8dd70153727
Vince Aquilina <vince.aquilina@protonmail.com>
2025-07-30 02:00:39 -0400

    use CalendarView type instead of string

--- client/src/components/calendar/CalendarToolbar.tsx
+++ client/src/components/calendar/CalendarToolbar.tsx
@@ -1,16 +1,19 @@
 import type { Setter } from 'solid-js';
 import { Temporal } from '@js-temporal/polyfill';
+
 import { CalendarNavigator } from './CalendarNavigator.tsx';
 import { ViewSwitcher } from './ViewSwitcher.tsx';
 import { UserDisplay } from '../UserDisplay.tsx';
+
+import type { CalendarView } from './ViewSwitcher.tsx';
 
 /** Toolbar component for the calendar screen header. */
 export function CalendarToolbar(
   props: {
     date: Temporal.PlainDate;
     setDate: Setter<Temporal.PlainDate>;
-    selectedView: string;
-    setSelectedView: Setter<string>;
+    selectedView: CalendarView;
+    setSelectedView: Setter<CalendarView>;
   },
 ) {
   return (
