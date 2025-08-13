a673e9957b73dbb706c98baecaaa24ac7cec6f19
Vince Aquilina <vince.aquilina@protonmail.com>
2025-08-13 01:24:55 -0400

    missing return

--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -2,19 +2,19 @@ import { createFileRoute, Outlet } from '@tanstack/solid-router';
 
 import { NavMenu } from '../components/nav/NavMenu.tsx';
 import { ThemeSwitcher } from '../components/ThemeSwitcher.tsx';
-import { NotFound } from '../components/feedback/NotFound.tsx';
 
 export const Route = createFileRoute('/app')({
   component: AppLayoutComponent,
-  notFoundComponent: NotFound,
 });
 
 function AppLayoutComponent() {
-  <div class='screen'>
-    <div class='logo-nav'>
-      <NavMenu />
-      <ThemeSwitcher />
+  return (
+    <div class='screen'>
+      <div class='logo-nav'>
+        <NavMenu />
+        <ThemeSwitcher />
+      </div>
+      <Outlet />
     </div>
-    <Outlet />
-  </div>;
+  );
 }
