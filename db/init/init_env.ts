af024b3d64db86d87d53542d4b0befa1ec76e177
Vince Aquilina <vince.aquilina@protonmail.com>
2025-08-25 20:26:41 -0400

    remove old env vars; add resend api key

--- db/init/init_env.ts
+++ db/init/init_env.ts
@@ -2,12 +2,11 @@ import * as path from '@std/path';
 
 /** Enum-like object containing environment variable keys. */
 export const ENV_VAR = {
-  SMTP_PORT: 'SMTP_PORT',
-  AUTH_PORT: 'AUTH_PORT',
   TURSO_DATABASE_URL: 'TURSO_DATABASE_URL',
   TURSO_AUTH_TOKEN: 'TURSO_AUTH_TOKEN',
   BETTER_AUTH_URL: 'BETTER_AUTH_URL',
   BETTER_AUTH_SECRET: 'BETTER_AUTH_SECRET',
+  RESEND_API_KEY: 'RESEND_API_KEY',
 } as const;
 
 /**
@@ -21,14 +20,14 @@ export const initEnv = () => {
 
   const db_credentials = Deno.readTextFileSync(path.resolve('..', 'db', 'db.txt'));
   const better_auth_env = Deno.readTextFileSync(path.resolve('..', 'db', 'ba.txt'));
+  const resend_api_key = Deno.readTextFileSync(path.resolve('..', 'db', 'rs.txt'));
 
   const [db_url, auth_token] = db_credentials.split('\n');
   const [ba_url, ba_secret] = better_auth_env.split('\n');
 
-  Deno.env.set(ENV_VAR.SMTP_PORT, '1025'); // replace
-  Deno.env.set(ENV_VAR.AUTH_PORT, '3001');
   Deno.env.set(ENV_VAR.TURSO_DATABASE_URL, db_url);
   Deno.env.set(ENV_VAR.TURSO_AUTH_TOKEN, auth_token);
   Deno.env.set(ENV_VAR.BETTER_AUTH_URL, ba_url);
   Deno.env.set(ENV_VAR.BETTER_AUTH_SECRET, ba_secret);
+  Deno.env.set(ENV_VAR.RESEND_API_KEY, resend_api_key);
 };
